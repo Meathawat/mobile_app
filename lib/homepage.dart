aa60f32a953a7da0b237fd08490597c944f8de7e
Gold <nonamegold@hotmail.com>
2024-12-21 13:28:02 +0700

    first commit

---
+++ lib/homepage.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  Widget build(BuildContext context) {
+    centerTitle: true
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test_1/homepage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+       
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyWidget(),
+    );
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("CPSU Project",
+        style: TextStyle(
+          color: Colors.white, fontSize: 28
+        ),
+       
+        ),
+        backgroundColor: const Color.fromARGB(255, 110, 84, 184)
+        ),
+        body: MyHomePage(),
+      );
+  }
+}
